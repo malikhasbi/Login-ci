300632f653a8e5d47c5c7616a250993e0e7a2244
malikhasbi <kakakiyayioto@gmail.com>
2022-07-15 09:48:43 +0700

    Commit

---
+++ application/helpers/my_helper.php
@@ -0,0 +1,24 @@
+<?php
+
+function check_login()
+{
+    $ci = get_instance();
+    if (!$ci->session->userdata('email')) {
+        redirect('auth');
+    } else {
+        $role_id = $ci->session->userdata('role_id');
+        $menu = $ci->uri->segment(1);
+
+        $queryMenu = $ci->db->get_where('user_menu', ['menu' => $menu])->row_array();
+        $menu_id = $queryMenu['id'];
+
+        $userAccsess = $ci->db->get_where('user_access_menu', [
+            'role_id' => $role_id,
+            'menu_id' => $menu_id
+        ]);
+
+        if ($userAccsess->num_rows() < 1) {
+            redirect('auth/blocked');
+        }
+    }
+}
