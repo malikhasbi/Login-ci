9c4b164fb4ec7aaf5693a8b4e5fe11cf9738f9f1
malikhasbi <kakakiyayioto@gmail.com>
2022-07-21 11:25:45 +0700

    committed

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -38,6 +38,29 @@ class User extends CI_Controller
             $name = $this->input->post('name');
             $email = $this->input->post('email');
 
+            // jika ada gambar di upload
+            $upload_image = $_FILES['image']['name'];
+
+            if ($upload_image) {
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size']     = '2048';
+                $config['upload_path'] = './assets/img/profile/';
+
+                $this->load->library('upload', $config);
+
+                if ($this->upload->do_upload('image')) {
+                    $old_image = $data['user']['image'];
+                    if ($old_image != 'default.jpg') {
+                        unlink(FCPATH . 'assets/img/profile/' . $old_image);
+                    }
+
+                    $new_image = $this->upload->data('file_name');
+                    $this->db->set('image', $new_image);
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
+
             $this->db->set('name', $name);
             $this->db->where('email', $email);
             $this->db->update('user');
--- application/views/user/index.php
+++ application/views/user/index.php
@@ -5,12 +5,12 @@
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
     <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-8">
             <?= $this->session->flashdata('message'); ?>
         </div>
     </div>
 
-    <div class="card mb-3" style="max-width: 540px;">
+    <div class="card mb-3 col-lg-5">
         <div class="row no-gutters">
             <div class="col-md-4">
                 <img class="card-img" src="<?= base_url('assets/img/profile/') . $user['image']; ?>">
